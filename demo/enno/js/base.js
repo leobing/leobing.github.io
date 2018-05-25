5371505f57279edf1862a905d164378314196c57
leobing <1724355376@qq.com>
2018-05-25 13:47:12 +0800

    fix huadou bug

--- demo/enno/js/base.js
+++ demo/enno/js/base.js
@@ -1,6 +1,39 @@
 $(function(){
-  var a = $('.laydate-day-mark').val();
-  console.log(a);
+
+	// 解决方案-切换
+	var solutions = $('.solution_items section');
+	for(var i=0; i<solutions.length; i++){
+		$(solutions[i]).click(function(event) {
+			$(solutions).removeClass('current');
+			$(solutions).find('dl').removeClass('current');
+
+			$(this).addClass('current');
+			$(this).find('dl').addClass('current');
+		});
+	}
+
+	// 日历
+	$('#calendar')
 })
 
-// lay-ymd="2018-5-16"
+
+
+
+// 工具-tab切换
+function tabSwitch(tab_index, tab_con){
+    //tab_index是切换索引
+    //tab_con是切换内容
+    for (var i = 0; i <= tab_index.length - 1; i++) {
+        tab_index[i].index = i;
+        $(tab_index[i]).bind('click',function(event){
+            
+            $(tab_con).css('display','none');
+            $(tab_con[this.index]).css('display','block');
+    
+            $(tab_index).removeClass('current');
+            $(this).addClass('current');
+            // console.log(this.index);
+        })
+    }
+}
+
